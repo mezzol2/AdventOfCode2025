db19dc6541ae0e5091f8839ede19c7233c33e737
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2025-12-26 20:56:25 +0900

    Complete Day 6 Part 2

--- day6/main.py
+++ day6/main.py
@@ -38,21 +38,74 @@ def part1_func(table:list) -> int:
     
     return total
 
-def part2_func(table:list) -> list:
-    pass
+def part2_func() -> int:
+    total = 0
+    table = list()
+    first = True
+
+    #build the table
+    with open("input.txt","r") as file:
+        for line in file:
+            i = 0
+
+            #not final line
+            if line[0] not in ["+","*"]:
+                for char in line:
+                    if first:
+                        table.append(list())
+                    table[i].append(char)
+                    i += 1
+            else: #final line
+                i = 0
+                add = True #arbitrary initial value
+                product = 0
+                for char in line:
+                    if char == "+":
+                        if not add:
+                            total += product
+                        add = True
+                    elif char == "*":
+                        if not add:
+                            total += product
+                        add = False
+                        product = 1
+                    
+                    if add and char != "\n":
+                        ceph_num = ""
+                        for digit in table[i]:
+                            if digit != " ":
+                                ceph_num += digit
+                        if ceph_num != "":
+                            total += int(ceph_num)
+                    elif char != "\n":
+                        ceph_num = ""
+                        for digit in table[i]:
+                            if digit != " ":
+                                ceph_num += digit
+                        if ceph_num != "":
+                            product *= int(ceph_num)
+
+
+                    i += 1
+
+                if not add:
+                    total += product
+
+            first = False
+
+    return total
 
 def main():
     start_time = time.perf_counter()
 
     table = read_file()
     part1 = part1_func(table)
+    part2 = part2_func()
 
     end_time = time.perf_counter()
 
     print(f"Part 1: {part1}")
-    # print(f"Part 2: {part2}")
+    print(f"Part 2: {part2}")
     print(f"Solution time: {end_time-start_time}")
-
-
 
 main()
