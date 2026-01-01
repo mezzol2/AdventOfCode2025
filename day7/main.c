d871fd8d27296faa481b0a21a77124b1fad28375
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2026-01-01 11:19:12 +0900

    Correct the inequality in part1Func

--- day7/main.c
+++ day7/main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 int part1Func(struct CharMatrix matrix);
+int shootBeam(struct CharMatrix matrix, int row, int col);
 
 int main(){
     //start timer
@@ -18,7 +19,7 @@ int main(){
     //end timer
     clock_t end = clock();
 
-    // printf("Part 1: %d\n", part1);
+    printf("Part 1: %d\n", part1);
     // printf("Part 2: %lld\n", part2);
     printf("Time: %f seconds", ((float)(end-start))/CLOCKS_PER_SEC);
 
@@ -28,7 +29,7 @@ int main(){
 int part1Func(struct CharMatrix matrix){
     //determine where the beam starts
     int start = 0;
-    while (matrix.charMatrix[0][start] =! 'S'){
+    while (matrix.charMatrix[0][start] != 'S'){
         start++;
     }
 
