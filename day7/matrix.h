1aaaa2da88794bb11c9e4173face6ca7742437ed
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2026-01-01 09:34:34 +0900

    Create CharMatrix struct and supporting functions

---
+++ day7/matrix.h
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct CharMatrix
+{
+    int numCol;
+    int numRow;
+    char** charMatrix;
+};
+
+
+int getNumCol(FILE *fptr){
+    //get the size of the line and number of columns
+    int numCol = 0;
+    char firstLine[200];
+    fgets(firstLine,200,fptr);
+    while(firstLine[numCol] != 10){  //check for a line break
+        numCol++;
+    }
+
+    //set the pointer back at the beginning of the file
+    fseek(fptr,0,0);
+
+    return numCol;
+}
+
+int getNumRow(FILE *fptr, int numCol){
+    //get size of the file
+    fseek(fptr,0,SEEK_END);
+    const int fileSize = ftell(fptr);
+    
+    //set the pointer back at the beginning of the file
+    fseek(fptr,0,0);
+
+    //determine the number of columns
+    //We add 1 to numCol to account for the line break characters
+    return fileSize / (numCol+1);
+}
+
+CharMatrix buildCharMatrix(FILE *fptr){
+    int i, j;
+
+    CharMatrix matrix;
+
+    matrix.numCol = getNumCol(fptr);
+    matrix.numRow = getNumRow(fptr, matrix.numCol);
+
+    //allocate memory for the rolls of paper matrix
+    matrix.charMatrix = (char**)malloc(matrix.numRow*sizeof(char*));
+    for (i = 0; i < matrix.numRow; i++){
+        matrix.charMatrix[i] = (char*) malloc(matrix.numCol*sizeof(char));
+    };
+ 
+    //build the matrix
+    fseek(fptr,0,0);
+    for (i = 0; i < matrix.numRow; i++){
+        for (j = 0; j < matrix.numCol; j++){
+            matrix.charMatrix[i][j] = fgetc(fptr);
+        }
+        //move pointer to beginning of the lext line
+        fgetc(fptr);
+    }
+
+    return matrix;
+}
+
+//for testing
+void printCharMatrix(CharMatrix matrix){
+    for (int i = 0; i < matrix.numRow; i++){
+        for (int j = 0; j < matrix.numCol; j++){
+            printf("%c",matrix.charMatrix[i][j]);
+        }
+    }
+}
