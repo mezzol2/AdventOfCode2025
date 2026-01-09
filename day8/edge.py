1b46d44c8b68d1629b0e47d0b944774452aa24b8
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2026-01-09 16:25:05 +0100

    Create create_all_edges functions and add Vertex class functions

--- day8/edge.py
+++ day8/edge.py
@@ -1,7 +1,14 @@
 import math
+from vertex import Vertex
 
 class Edge:
-    def __init__(self, node1, node2):
+    def __init__(self, node1:Vertex, node2:Vertex):
         self.node1 = node1
         self.node2 = node2
         self.dist = math.sqrt((node1.x - node2.x)**2+(node1.y - node2.y)**2+(node1.z - node2.z)**2)
+
+    def __eq__(self, other):
+        return (self.node1 == other.node1 and self.node2 == other.node2) or (self.node1 == other.node2 and self.node2 == other.node1)
+    
+    def __hash__(self):
+        return hash((self.node1,self.node2))
--- day8/main.py
+++ day8/main.py
@@ -1,22 +1,34 @@
 from vertex import Vertex
+from edge import Edge
 
 def main():
     fileName = "test.txt"
-    edge_set = set()
     vertex_set = set()
+    edge_set = set()
 
     #read the file and create the set of vertices
     read_file(vertex_set,fileName)
 
-    for v in vertex_set:
-        print(f"{v.x} {v.y} {v.z}")
+    create_all_edges(vertex_set, edge_set)
+
+    print("hello")
 
 
+#O(V)
 def read_file(vertex_set:set, fileName:str):
     with open(fileName, "r") as file:
         for line in file:
             line = line.strip().split(",")
             #add vertices to the vertex_set
             vertex_set.add( Vertex(int(line[0]),int(line[1]),int(line[2])) )
+
+#O(V^2)
+def create_all_edges(vertex_set:set, edge_set:set):
+    for v in vertex_set:
+        for u in vertex_set:
+            if v != u:
+                edge = Edge(v,u)
+                if edge not in edge_set:
+                    edge_set.add(edge)
 
 main()
